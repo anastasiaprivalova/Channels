6ebdbe37488cee9396ff01bfb7af5a2b2e3cf8b6
Anastasiya Pryvalava <Anastasiya Pryvalava_epam.com>
2021-04-28 20:42:51 +0300

    =Configure deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const tailwindcss = require('tailwindcss');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/Channels/' : '/',
     css: {
       loaderOptions: {
         postcss: {
