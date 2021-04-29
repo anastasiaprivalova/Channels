f624f528079710042b29a9303d5743f6bec62f8f
Anastasiya Pryvalava <Anastasiya Pryvalava_epam.com>
2021-04-29 23:14:34 +0300

    =describe main flow in  integration tests

--- cypress/integration/channels_cypress_test.spec.js
+++ cypress/integration/channels_cypress_test.spec.js
@@ -53,9 +53,44 @@ describe('Channels', () => {
     cy.get('[test-id="apply-btn"]')
       .should('be.visible')
 
-    // Check if channel with icon was added
-    cy.get('.list-group li svg')
+    // Check if new channel with icon was added
+    cy.get('.list-group li .bg-galaxy-100 svg')
       .its('length')
       .should('be.eq', 1)
+
+    // Apply
+    cy.get('[test-id="apply-btn"]')
+      .first()
+      .click()
+
+    // Select another option
+    cy.get('[test-id="add-input"]')
+      .type('t')
+    cy.get('[test-id="add-options"]>li')
+      .first()
+      .click()
+
+    // Check if channel was added
+    cy.get('.list-group li')
+      .its('length')
+      .should('be.eq', 2)
+
+    // Delete first option
+    cy.get('.list-group li:first-of-type button')
+      .first()
+      .click()
+
+    // Check if first option was deleted
+    cy.get('.list-group li:first-of-type .bg-galaxy-100 + span')
+      .should('not.have.text', 'Instagram')
+
+    // Rollback
+    cy.get('[test-id="cancel-btn"]')
+      .first()
+      .click()
+
+    // Check if applied channel still exists
+    cy.get('.list-group li:first-of-type .bg-galaxy-100 + span')
+      .should('have.text', 'Instagram')
   })
 })
